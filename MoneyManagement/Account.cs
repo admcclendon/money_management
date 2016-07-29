5240ef14eb61102a0aed6a2435d6919a509bea86
Alex McClendon <admcclendon@gmail.com>
2016-07-29 17:07:48 -0400

    Added an account data structure. This is displayed in a listbox on the main window.

---
+++ MoneyManagement/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(string name, double balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+    }
+
+    public class AccountCollection : List<Account>
+    {
+
+    }
+}
---
+++ MoneyManagement/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagement
+{
+    public class MainWindowViewModel
+    {
+        public AccountCollection Accounts { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Accounts = new AccountCollection();
+            Accounts.Add(new Account("Checking", 1234.56));
+            Accounts.Add(new Account("Savings", 4321.45));
+        }
+    }
+}
